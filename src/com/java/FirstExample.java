a8f55670fa8d5f57bc38abc1320c4dab08101603
azizimisri <azizimisri@gmail.com>
2022-02-13 11:59:15 -0500

    New design for home page

--- src/com/java/FirstExample.java
+++ src/com/java/FirstExample.java
@@ -7,6 +7,7 @@ public class FirstExample {
 		System.out.println("Login Page Created");
 		System.out.println("Enter your username and password");
 		System.out.println("Home page created");
+		System.out.println("Designed Home Page");
 		
 		
 	}
